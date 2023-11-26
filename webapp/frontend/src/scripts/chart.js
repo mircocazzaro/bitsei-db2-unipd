569db080612eb49f3b0937348f98e9f3924f2788
Farzad Shami <farzad.shami@studenti.unipd.it>
2023-11-26 10:45:54 +0100

    fix(webapp): the issue of re-using the chart canvas + dynamic loading the text and details

--- webapp/frontend/src/scripts/chart.js
+++ webapp/frontend/src/scripts/chart.js
@@ -1,32 +1,28 @@
 import Chart from "chart.js/auto";
 
 function loadQueryOne() {
-    fetch("http://127.0.0.1:8080/api/v1/business/")
+    return fetch("http://127.0.0.1:8080/api/v1/business/")
         .then(response => response.json())
-        .then(data => {
-            new Chart(
-                document.getElementById('chart'),
-                {
-                    type: 'bar',
-                    data: {
-                        labels: data.map(row => row.cityName),
-                        datasets: [
-                            {
-                                label: '2020',
-                                data: data.map(row => row.numBusinesses20),
-                            }, {
-                                label: '2021',
-                                data: data.map(row => row.numBusinesses21),
-                            }, {
-                                label: '2022',
-                                data: data.map(row => row.numBusinesses22),
-                            }
-                        ]
-                    }
+        .then(data => (
+            {
+                type: 'bar',
+                data: {
+                    labels: data.map(row => row.cityName),
+                    datasets: [
+                        {
+                            label: '2020',
+                            data: data.map(row => row.numBusinesses20),
+                        }, {
+                            label: '2021',
+                            data: data.map(row => row.numBusinesses21),
+                        }, {
+                            label: '2022',
+                            data: data.map(row => row.numBusinesses22),
+                        }
+                    ]
                 }
-            );
-        })
-        .catch(error => {
+            }
+        )).catch(error => {
             console.error(error)
         });
 }
