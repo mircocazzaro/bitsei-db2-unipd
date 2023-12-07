4d7fa4a99a176e7b220e2eaefedc0030e306f0c0
Mirco Cazzaro <mirco.cazzaro@studenti.unipd.it>
2023-12-07 16:03:53 +0100

    added tables & correct 8th query

--- queries/5/crime_dist_by_cat_over_period.py
+++ queries/5/crime_dist_by_cat_over_period.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 # Read the CSV file into a DataFrame
 df = pd.read_csv('5b.csv')
@@ -13,28 +14,36 @@ violent_crimes = df['numViolentCrimes']
 public_order_crimes = df['numPublicOrderCrimes']
 
 # Set up the bar positions
-bar_width = 0.15
+bar_width = 0.5
 index = range(len(periods))
 
-# Plot the bars
-plt.figure(figsize=(20, 10))
-plt.bar(index, sexual_crimes, width=bar_width, label='Sexual Crimes')
-plt.bar([i + bar_width for i in index], property_crimes, width=bar_width, label='Property Crimes')
-plt.bar([i + 2 * bar_width for i in index], white_collar_crimes, width=bar_width, label='White Collar Crimes')
-plt.bar([i + 3 * bar_width for i in index], violent_crimes, width=bar_width, label='Violent Crimes')
-plt.bar([i + 4 * bar_width for i in index], public_order_crimes, width=bar_width, label='Public Order Crimes')
+# Plot the stacked bars
+plt.figure(figsize=(20, 10)) 
+plt.bar(index, property_crimes, width=bar_width, label='Property Crimes')
+plt.bar(index, violent_crimes, width=bar_width, label='Violent Crimes', bottom=property_crimes)
+plt.bar(index, public_order_crimes, width=bar_width, label='Public Order Crimes', bottom=violent_crimes + property_crimes)
+plt.bar(index, white_collar_crimes, width=bar_width, label='White Collar Crimes', bottom=property_crimes + public_order_crimes + violent_crimes)
+plt.bar(index, sexual_crimes, width=bar_width, label='Sexual Crimes', bottom=property_crimes + public_order_crimes + violent_crimes + white_collar_crimes)
+
+# Plot the line plot interpolating the top of each column
+#top_interpolated = np.sum([sexual_crimes, property_crimes, white_collar_crimes, violent_crimes, public_order_crimes], axis=0)
+#plt.plot(index, top_interpolated, color='black', marker='o', linestyle='-', linewidth=2, markersize=8, label='Top Interpolated')
+
+# Interpolate the top of each column for a smoother line
+top_interpolated = np.sum([sexual_crimes, property_crimes, white_collar_crimes, violent_crimes, public_order_crimes], axis=0)
+top_interpolated_smooth = np.interp(np.linspace(0, len(index)-1, 100), index, top_interpolated)
+
+# Plot the smoothed line
+plt.plot(np.linspace(0, len(index)-1, 100), top_interpolated_smooth, color='black', linewidth=2, label='Top Interpolated')
+
+
 
 # Customize the plot
 plt.xlabel('Periods')
 plt.ylabel('Number of Crimes')
-plt.title('Crime Statistics Over Different Periods')
-plt.xticks([i + 2 * bar_width for i in index], periods)
+plt.title('Stacked Crime Statistics Over Different Periods')
+plt.xticks(index, periods, rotation=70, ha='right')  # Rotate labels and set alignment
 plt.legend()
-
-
-plt.xticks(rotation=45)  # Rotate x-axis labels for better readability
 plt.grid(True)
-plt.subplots_adjust(bottom=0.2)
-# Save the plot as a high-resolution image (1080p)
+plt.subplots_adjust(bottom=0.25)
 plt.savefig('crime_dist_by_cat_over_period.png', dpi=300)
-
